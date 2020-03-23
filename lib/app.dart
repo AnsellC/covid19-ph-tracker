f42c332436c06a4701635e2e1d6e662a863bc651
AnsellC <ansellcruz@gmail.com>
2020-03-24 05:59:27 +0800

    change to portrait only

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,17 @@
 import 'package:covid/providers/stats_provider.dart';
 import 'package:covid/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class CovidApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Covid PH',
